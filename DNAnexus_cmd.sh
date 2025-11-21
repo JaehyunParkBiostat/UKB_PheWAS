822277f5074325484498abb173a521cd5600e037
Jaehyun Park <56022547+JaehyunParkBiostat@users.noreply.github.com>
2025-11-21 17:05:33 -0600

    Update DNAnexus_cmd.sh

--- DNAnexus_cmd.sh
+++ DNAnexus_cmd.sh
@@ -19,6 +19,12 @@ dx upload --path '/PheWAS/Data/b37ToHg38.over.chain' b37ToHg38.over.chain
 dx upload --path '/PheWAS/Data/1000genomes.grch38.fasta-index.tar.gz' 1000genomes.grch38.fasta-index.tar.gz
 
 dx run workflow-J4QKVJ0Jf7xyYfyZg837BjFY
+# stage-common.plink_beds[0]: Bulk/Genotype\ Results/Genotype\ calls/ukb22418_c21_b0_v2.bed
+# stage-common.plink_bims[0]: Bulk/Genotype\ Results/Genotype\ calls/ukb22418_c21_b0_v2.bim
+# stage-common.plink_fams[0]: Bulk/Genotype\ Results/Genotype\ calls/ukb22418_c21_b0_v2.fam
+# stage-common.reference_fastagz: PheWAS/Data/1000genomes.grch38.fasta-index.tar.gz
+# stage-common.ucsc_chain: PheWAS/Data/b37ToHg38.over.chain
+
 # Analysis ID: analysis-J4QKvY8Jf7xY6pxPj7GZXKX8
 
 # Alternatively, use a JSON input and run the command below:
